c3b1880706efe9ad16bd8ac32d4796ea8be4f6b9
Study-JavaScript <sektersytout@gmail.com>
2024-07-15 10:30:59 +0200

    refactor(test/cli): Clean Code-> delete comments

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,3 @@
-// export function debounce<DebounceFunction extends (...args: any[])=>void>(func:DebounceFunction, wait: number): (...args: Parameters<DebounceFunction>)=>void{
-//     let timeout: ReturnType<typeof setTimeout>;
-//     return function(this:unknown, ...args: Parameters<DebounceFunction>){
-        
-//         clearTimeout(timeout);
-//         timeout = setTimeout(() =>{
-//             func.apply(this, args);
-        
-//         }, wait);
-//     };
-
-// }
-
-
-
-
 export function debounce<Args extends any[]>(callback: (...args: Args) => void, wait: number): (...args: Args) => void {
     let timerId: ReturnType<typeof setTimeout>;
     return (...args: Args) => {
